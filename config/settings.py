6be84df899893d31ac3adb3e870907851262f495
Caner Cevik <cnrcvk7@gmail.com>
2026-08-24 15:01:25 +0300

    fixed admin panel

--- config/settings.py
+++ config/settings.py
@@ -47,7 +47,7 @@ ALLOWED_HOSTS: list[str] = [
 
 #  Applications
 INSTALLED_APPS: list[str] = [
-    "unfold",
+  #  "unfold",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
